55c11a0d0bc9401f55dadf67caf83167b0411b3d
Arthur Wallendorff <howdy@wallenart.dev>
2025-12-30 18:40:39 +0100

    feat: add germain

---
+++ hosts/germain/configuration.nix
@@ -0,0 +1,48 @@
+{ pkgs, inputs, ... }:
+
+{
+  imports = [
+    ./hardware-configuration.nix
+    ./networking.nix
+  ];
+
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  boot.initrd.kernelModules = [ "virtio_gpu" ];
+  boot.kernelParams = [ "console=tty" ];
+  boot.tmp.cleanOnBoot = true;
+
+  zramSwap.enable = true;
+
+  networking.hostName = "germain";
+  networking.domain = "";
+
+  time.timeZone = "Europe/Brussels";
+
+  users.users.root.openssh.authorizedKeys.keys = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAShO/JDmlKC2RAVURPnSUpc+EQrj2/C2PrKeHbChkte lab@x1"
+  ];
+
+  services.openssh = {
+    enable = true;
+    settings = {
+      PermitRootLogin = "prohibit-password";
+      PasswordAuthentication = false;
+    };
+  };
+
+  home-manager = {
+    extraSpecialArgs = { inherit inputs; };
+    users.root = import ./home.nix;
+  };
+
+  environment.systemPackages = with pkgs; [
+    neovim
+    btop
+    git
+    curl
+    ripgrep
+  ];
+
+  system.stateVersion = "25.11";
+}
