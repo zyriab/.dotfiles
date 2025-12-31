42cca1b5fb3e9759baf3d2a34f70dd028729119d
Arthur Wallendorff <howdy@wallenart.dev>
2025-12-31 08:38:32 +0100

    chore: fix warning

--- hosts/xenia/configuration.nix
+++ hosts/xenia/configuration.nix
@@ -40,7 +40,7 @@
     enable = true;
     settings = {
       default_session = {
-        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --remember --cmd Hyprland";
+        command = "${pkgs.tuigreet}/bin/tuigreet --time --remember --cmd Hyprland";
         user = "greeter";
       };
     };
