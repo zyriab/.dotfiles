940db3f0e924da30f410408e77b55c83822f7758
Arthur Wallendorff <howdy@wallenart.dev>
2025-12-31 13:04:32 +0100

    chore: add hcloud

--- modules/home/dev.nix
+++ modules/home/dev.nix
@@ -1,47 +1,55 @@
-{ pkgs, lib, inputs, ... }:
+{
+  pkgs,
+  lib,
+  inputs,
+  ...
+}:
 let
   isX86 = pkgs.stdenv.hostPlatform.system == "x86_64-linux";
 in
 {
   programs.go.enable = true;
 
-  home.packages = with pkgs; [
-    # CLI tools
-    inputs.claude-code.packages.${stdenv.hostPlatform.system}.default
-    cloc
+  home.packages =
+    with pkgs;
+    [
+      # CLI tools
+      inputs.claude-code.packages.${stdenv.hostPlatform.system}.default
+      cloc
 
-    # Languages & runtimes
-    nodejs
-    lua
-    luarocks
-    cargo
+      # Languages & runtimes
+      nodejs
+      lua
+      luarocks
+      cargo
 
-    # Compilers & build tools
-    gcc
-    gnumake
-    pkg-config
-    glibc
-    libconfig
+      # Compilers & build tools
+      gcc
+      gnumake
+      pkg-config
+      glibc
+      libconfig
 
-    # Formatters & linters
-    gofumpt
-    golines
-    golangci-lint
-    stylua
-    prettierd
+      # Formatters & linters
+      gofumpt
+      golines
+      golangci-lint
+      stylua
+      prettierd
 
-    # Nix
-    nixd
-    nixfmt-rfc-style
-    nix-search-cli
+      # Nix
+      nixd
+      nixfmt-rfc-style
+      nix-search-cli
 
-    # Networking/tunnels
-    cloudflared
-  ]
-  # x86-only packages
-  ++ lib.optionals isX86 [
-    inputs.opencode.packages.${stdenv.hostPlatform.system}.default
-    postman
-    ngrok
-  ];
+      # Networking/tunnels/VPS
+      cloudflared
+      hcloud
+    ]
+    # x86-only packages
+    ++ lib.optionals isX86 [
+      inputs.opencode.packages.${stdenv.hostPlatform.system}.default
+      postman
+      ngrok
+    ];
 }
