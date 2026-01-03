085e530fa12c328906a45f4280d2729906c845f0
Arthur Wallendorff <howdy@wallenart.dev>
2026-01-03 11:50:12 +0100

    fix(basil): use `start-hyprland`

--- hosts/basil/configuration.nix
+++ hosts/basil/configuration.nix
@@ -124,7 +124,7 @@
     enable = true;
     settings = {
       default_session = {
-        command = "${pkgs.tuigreet}/bin/tuigreet --time --remember --cmd Hyprland";
+        command = "${pkgs.tuigreet}/bin/tuigreet --time --remember --cmd start-hyprland";
         user = "greeter";
       };
     };
