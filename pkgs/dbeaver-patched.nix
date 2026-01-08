5b7e0d6b4a43a3e18994715d5c222a72b124239a
Arthur Wallendorff <howdy@wallenart.dev>
2026-01-08 14:32:35 +0100

    chore: remove dbeaver

--- pkgs/dbeaver-patched.nix
+++
@@ -1,8 +0,0 @@
-{ pkgs }:
-with pkgs;
-dbeaver-bin.overrideAttrs (oldAttrs: {
-  buildInputs = (oldAttrs.buildInputs or [ ]);
-  postInstall = (oldAttrs.postInstall or "") + ''
-    wrapProgram $out/bin/dbeaver --set GDK_BACKEND "x11";
-  '';
-})
